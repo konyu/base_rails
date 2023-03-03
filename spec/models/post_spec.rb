ee92d1e7c859ea1e52ff3c7abfe574c61b257477
konyu <yusukedrive@gmail.com>
2023-03-03 11:01:46 +0900

    Fix: Rails and Rspec debug settings

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "#sample" do
     it "first test" do
-      expect(Post.new.sample).to eq("SAMPLE")
+      debugger # VSCodeのデバッガでは止まらない
+      expect(Post.new.sample).to eq("SAMPLE") # VSCodeのブレークポイントで止まる
+      binding.b # VSCodeのデバッガでも止まる
     end
   end
 end
